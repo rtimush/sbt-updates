87214b0645526576ec088a422ba16ee796e7a8cb
xuwei-k <6b656e6a69@gmail.com>
2012-12-25 03:10:16 +0900

    update dependencies

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,7 +2,7 @@ resolvers ++= Seq(
   "less is" at "http://repo.lessis.me",
   "coda" at "http://repo.codahale.com")
 
-addSbtPlugin("net.virtual-void" % "sbt-cross-building" % "0.7.0-RC2")
+addSbtPlugin("net.virtual-void" % "sbt-cross-building" % "0.7.0")
 
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.1.0")
 
--- src/test/scala/com/timushev/sbt/updates/FixedMetadataLoader.scala
+++ src/test/scala/com/timushev/sbt/updates/FixedMetadataLoader.scala
@@ -1,10 +1,10 @@
 package com.timushev.sbt.updates
 
 import sbt.ModuleID
-import dispatch.Promise
+import dispatch.{Http, Promise}
 import versions.Version
 
 class FixedMetadataLoader(available: Seq[String]) extends MetadataLoader {
   def getVersions(module: ModuleID): Promise[Seq[Version]] =
-    Promise(available.map(Version.apply))
+    Http.promise(available.map(Version.apply))
 }
