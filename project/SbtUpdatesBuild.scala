f2bda5801bc8578bb266433e787d1582e8790566
Jacek Laskowski <jacek@japila.pl>
2014-04-16 23:37:55 +0200

    sbt 0.13.2 + some look-and-feel improvements

--- project/SbtUpdatesBuild.scala
+++ project/SbtUpdatesBuild.scala
@@ -5,8 +5,8 @@ object SbtUpdatesBuild extends Build {
   val publishMinJar = TaskKey[File]("publish-min-jar")
   val Embedded = config("embedded").hide
 
-  lazy val root = Project(id = "sbt-updates", base = file("."), settings = Project.defaultSettings)
-    .configs(Embedded)
-    .settings(inConfig(Embedded)(Defaults.configSettings): _*)
-
+  lazy val `sbt-updates` = project in file(".") settings(
+    Project.defaultSettings ++
+    inConfig(Embedded)(Defaults.configSettings): _*
+  ) configs Embedded
 }
