69fbff071ed61bc4ca116ecc2bbeb124fce704bc
Roman Timushev <rtimush@gmail.com>
2014-11-09 20:03:45 +0300

    Make keys auto-imported

--- src/main/scala-sbt-0.13/com/timushev/sbt/updates/UpdatesKeys.scala
+++ src/main/scala-sbt-0.13/com/timushev/sbt/updates/UpdatesKeys.scala
@@ -5,10 +5,12 @@ import sbt._
 
 import scala.collection.immutable.SortedSet
 
-object UpdatesKeys {
+trait UpdatesKeys {
   lazy val dependencyUpdatesReportFile = settingKey[File]("Dependency updates report file")
   lazy val dependencyUpdatesFailBuild = settingKey[Boolean]("Fail a build if updates found")
   lazy val dependencyUpdatesData = taskKey[Map[ModuleID, SortedSet[Version]]]("")
   lazy val dependencyUpdates = taskKey[Unit]("Shows a list of project dependencies that can be updated.")
   lazy val dependencyUpdatesReport = taskKey[File]("Writes a list of project dependencies that can be updated to a file.")
 }
+
+object UpdatesKeys extends UpdatesKeys
--- src/main/scala/com/timushev/sbt/updates/UpdatesPlugin.scala
+++ src/main/scala/com/timushev/sbt/updates/UpdatesPlugin.scala
@@ -4,7 +4,7 @@ import sbt._
 import sbt.Keys._
 import com.timushev.sbt.updates.UpdatesKeys._
 
-object UpdatesPlugin extends Plugin with UpdatesPluginTasks {
+object UpdatesPlugin extends Plugin with UpdatesPluginTasks with UpdatesKeys {
 
   override val projectSettings = Seq(
     dependencyUpdatesReportFile := target.value / "dependency-updates.txt",
