03cd5afe0da49b234d9b45051ca61a26bf45d117
Roman Timushev <rtimush@gmail.com>
2016-11-15 00:36:05 +0300

    Fix caching for multiple repositories

--- src/main/scala/com/timushev/sbt/updates/metadata/MetadataLoaderFactory.scala
+++ src/main/scala/com/timushev/sbt/updates/metadata/MetadataLoaderFactory.scala
@@ -13,7 +13,13 @@ object MetadataLoaderFactory {
 
   val KnownProtocol = "(?i)^https?$".r
 
-  def loader(logger: Logger, credentials: Seq[Credentials]): PartialFunction[Resolver, MetadataLoader] = Function.unlift[Resolver, MetadataLoader]({
+  def loader(logger: Logger, credentials: Seq[Credentials]): PartialFunction[Resolver, MetadataLoader] = {
+    Function.unlift { resolver =>
+      loaderCache.getOrElseUpdate(resolver, newLoader(logger, credentials, resolver).map(cached))
+    }
+  }
+
+  private def newLoader(logger: Logger, credentials: Seq[Credentials], resolver: Resolver) = resolver match {
     case repo: MavenRepository =>
       val downloader = new Downloader(credentials, logger)
       val url = new URL(repo.root)
@@ -23,9 +29,10 @@ object MetadataLoaderFactory {
       }
     case _ =>
       None
-  }).andThen(cached)
+  }
 
-  private val cache = mutable.Map[ModuleID, Future[Seq[Version]]]()
+  private val loaderCache = mutable.Map[Resolver, Option[MetadataLoader]]()
+  private val cache = mutable.Map[(MetadataLoader, ModuleID), Future[Seq[Version]]]()
 
   private def cached(loader: MetadataLoader) = new CachingMetadataLoader(loader, cache)
 
