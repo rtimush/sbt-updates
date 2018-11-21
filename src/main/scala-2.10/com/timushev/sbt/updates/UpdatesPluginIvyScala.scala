4bec79ebb74d2a852b16d8e58d6d0a2b1e8b0676
Ivan Stanislavciuc <ivan.stanislavciuc@vimpelcom.com>
2018-11-21 07:59:06 +0100

    Fix typo

--- src/main/scala-2.10/com/timushev/sbt/updates/UpdatesPluginIvyScala.scala
+++ src/main/scala-2.10/com/timushev/sbt/updates/UpdatesPluginIvyScala.scala
@@ -8,7 +8,7 @@ object UpdatesPluginIvyScala extends AutoPlugin {
   override def trigger = allRequirements
 
   lazy val dependencyUpdatesIvtScala =
-    taskKey[Option[sbt.IvyScala]]("A task that maps to scalaModuleInfo in scala 2.10")
+    taskKey[Option[sbt.IvyScala]]("A task that maps to ivyScala in scala 2.10")
 
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
     dependencyUpdatesIvtScala := ivyScala.value
