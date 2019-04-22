8182562a74f6b84a1294d3ceec9f7909729abd42
kiranbayram <kiranbayram@gmail.com>
2019-04-22 16:19:27 +0200

    Add tests for plus patch versions

--- src/test/scala/com/timushev/sbt/updates/UpdatesFinderSpec.scala
+++ src/test/scala/com/timushev/sbt/updates/UpdatesFinderSpec.scala
@@ -26,7 +26,8 @@ class UpdatesFinderSpec extends FreeSpec with Matchers {
     "1.0.0",
     "1.0.1-SNAPSHOT",
     "1.0.1-M3",
-    "1.0.1"
+    "1.0.1",
+    "1.1.0",
   )
 
   "An updates finder" - {
@@ -95,39 +96,56 @@ class UpdatesFinderSpec extends FreeSpec with Matchers {
     "for stable artifacts" - {
       val u = updates("1.0.0", available, allowPreRelease = false)
       val pu = updates("1.0.0", available, allowPreRelease = true)
+      val uPlus = updates("1.0.+", available, allowPreRelease = false)
       "should not show old stable versions" in {
         u should not(contain("0.9.9"))
         pu should not(contain("0.9.9"))
+        uPlus should not(contain("0.9.9"))
       }
       "should not show old snapshots" in {
         u should not(contain("0.9.9-SNAPSHOT"))
         pu should not(contain("0.9.9-SNAPSHOT"))
+        uPlus should not(contain("0.9.9-SNAPSHOT"))
       }
       "should not show old milestones" in {
         u should not(contain("0.9.9-M3"))
         pu should not(contain("0.9.9-M3"))
+        uPlus should not(contain("0.9.9-M3"))
       }
       "should not show current snapshot" in {
         u should not(contain("1.0.0-SNAPSHOT"))
         pu should not(contain("1.0.0-SNAPSHOT"))
+        uPlus should not(contain("1.0.0-SNAPSHOT"))
       }
       "should not show current milestones" in {
         u should not(contain("1.0.0-M3"))
         pu should not(contain("1.0.0-M3"))
+        uPlus should not(contain("1.0.0-M3"))
       }
       "should show stable updates" in {
         u should contain("1.0.1")
+        u should contain("1.1.0")
         pu should contain("1.0.1")
+        pu should contain("1.1.0")
       }
       "should not show snapshot updates" in {
         u should not(contain("1.0.1-SNAPSHOT"))
         pu should not(contain("1.0.1-SNAPSHOT"))
+        uPlus should not(contain("1.0.1-SNAPSHOT"))
       }
       "should not show milestone updates" in {
         u should not(contain("1.0.1-M3"))
+        uPlus should not(contain("1.0.1-M3"))
       }
       "should show milestone updates when allowing pre-releases" in {
         pu should contain("1.0.1-M3")
+      }
+      "should show minor updates when using plus patch version" in {
+        uPlus should contain("1.1.0")
+      }
+      "should not show patch updates when using plus patch version" in {
+        uPlus should not(contain("1.0.0"))
+        uPlus should not(contain("1.0.1"))
       }
     }
   }
