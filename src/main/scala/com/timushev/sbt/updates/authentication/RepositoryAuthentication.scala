94238e573b9000e7f2495958c0914cdbb463db9b
Roman Timushev <rtimush@gmail.com>
2021-12-05 16:51:31 +0100

    Support authentication headers from csrConfiguration

---
+++ src/main/scala/com/timushev/sbt/updates/authentication/RepositoryAuthentication.scala
@@ -0,0 +1,61 @@
+package com.timushev.sbt.updates.authentication
+
+import com.timushev.sbt.updates.authentication.Coursier.CoursierConfiguration
+import sbt._
+
+import scala.language.reflectiveCalls
+import scala.util.control.Exception.allCatch
+
+case class RepositoryAuthentication(
+    repositoryId: Option[String],
+    realm: Option[String],
+    host: Option[String],
+    user: String,
+    password: String,
+    headers: Seq[(String, String)]
+) {
+  def describe: String =
+    (if (user.nonEmpty || password.nonEmpty) s"$user:***** " else "") +
+      headers.map { case (k, _) => s"$k:*****" }.mkString("(", ", ", ")")
+}
+
+object RepositoryAuthentication {
+
+  def fromCredentials(c: Credentials): Option[RepositoryAuthentication] =
+    c match {
+      case d: DirectCredentials =>
+        Some(
+          RepositoryAuthentication(
+            repositoryId = None,
+            realm = Some(d.realm),
+            host = Some(d.host),
+            user = d.userName,
+            password = d.passwd,
+            headers = Nil
+          )
+        )
+      case _ => None
+    }
+
+  def fromCoursier(c: CoursierConfiguration): Seq[RepositoryAuthentication] =
+    c.authenticationByRepositoryId.flatMap { case (repositoryId, auth) =>
+      allCatch.opt {
+        RepositoryAuthentication(
+          repositoryId = Some(repositoryId),
+          realm = auth.realmOpt,
+          host = None,
+          user = auth.user,
+          password = auth.password,
+          headers = auth.headers
+        )
+      }
+    }
+
+  def find(
+      host: String,
+      repositoryId: String,
+      authentications: Seq[RepositoryAuthentication]
+  ): Option[RepositoryAuthentication] =
+    authentications.find(a => a.host == Some(host) || a.repositoryId == Some(repositoryId))
+
+}
