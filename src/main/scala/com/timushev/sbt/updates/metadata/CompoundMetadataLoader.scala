9bee0f47af7fc1f1daddaa7a5ee52d0aacb3420e
Roman Timushev <rtimush@gmail.com>
2022-05-30 01:10:43 +0200

    Extract versions from maven central html listing

--- src/main/scala/com/timushev/sbt/updates/metadata/CompoundMetadataLoader.scala
+++ src/main/scala/com/timushev/sbt/updates/metadata/CompoundMetadataLoader.scala
@@ -7,9 +7,11 @@ import scala.concurrent.Future
 
 class CompoundMetadataLoader(loaders: Seq[MetadataLoader]) extends MetadataLoader {
   override def getVersions(module: sbt.ModuleID): Future[Seq[Version]] =
-    loaders.foldLeft(Future.failed[Seq[Version]](new NoSuchElementException)) { (result, loader) =>
-      result.recoverWith { case _ =>
-        loader.getVersions(module)
+    Future
+      .sequence {
+        loaders.map { loader =>
+          loader.getVersions(module).recover { case _ => Seq.empty }
+        }
       }
-    }
+      .map(_.flatten)
 }
