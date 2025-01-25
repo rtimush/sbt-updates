c352fdeadf36451ca50e9e94b2e3221d2654cdaf
Scala Steward <scala_steward@virtuslab.com>
2025-01-25 03:30:52 +0000

    Reformat with scalafmt 3.8.6
    Executed command: scalafmt --non-interactive

--- src/test/scala/com/timushev/sbt/updates/Base64Spec.scala
+++ src/test/scala/com/timushev/sbt/updates/Base64Spec.scala
@@ -4,8 +4,7 @@ import org.scalatest.freespec.AnyFreeSpec
 
 class Base64Spec extends AnyFreeSpec {
   "Base64 encoder" - {
-    "should encode data" in {
+    "should encode data" in
       assert(Base64.encodeToString(Array[Byte](1, 127)) === "AX8=")
-    }
   }
 }
